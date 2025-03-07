ad17f0cd9876c674885799faab45c7531521d826
Bernadette Modi <berniemain353@gmail.com>
2025-03-07 20:25:27 +0300

    Defined the /start command so that when a user types that the bor replies with a welcome message.

--- dailyquote.py
+++ dailyquote.py
@@ -9,3 +9,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Hey there! 💖 I'm your Quote Bot. Use /quote  to get a random quote!")
+    
