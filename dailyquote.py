5cc859862e52636879d6f703debb4f8bfbec1c66
Bernadette Modi <berniemain353@gmail.com>
2025-03-07 20:22:32 +0300

    Setting up logging do that helps debug if anything goes wrong.

--- dailyquote.py
+++ dailyquote.py
@@ -4,3 +4,8 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
  
 TOKEN = "8054309658:AAEeNQf1GRlHgrAnxGcuHB8bnX8y2_Kngqk"
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
